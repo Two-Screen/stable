5da5fbd3bc3626e12a92546c0bf6459a845811c7
Stéphan Kochen <stephan@kochen.nl>
2017-03-15 17:31:37 +0100

    Version 0.1.6.

--- stable.js
+++ stable.js
@@ -1,5 +1,5 @@
-//! stable.js 0.1.5, https://github.com/Two-Screen/stable
-//! © 2014 Angry Bytes and contributors. MIT licensed.
+//! stable.js 0.1.6, https://github.com/Two-Screen/stable
+//! © 2017 Angry Bytes and contributors. MIT licensed.
 
 (function() {
 
